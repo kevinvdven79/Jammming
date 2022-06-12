d221084f87872ea94a4cfed800eeee70a9c9b57d
Kevin van der Ven <44703330+kevinvdven79@users.noreply.github.com>
2022-06-12 10:46:32 +0200

    nog twee stappen te gaan

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component{
   constructor(props){
@@ -20,6 +21,8 @@ class App extends React.Component{
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track){
@@ -43,16 +46,27 @@ class App extends React.Component{
 
   }
 
+  savePlaylist(){
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(keyWord){
+    Spotify.search(keyWord).then(searchResults =>{
+      this.setState(searchResults: searchResults)
+    })
+  }
+
   render(){
     return(
       <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    <SearchBar />
+    <SearchBar onSearch={this.search}/>
     <div className="App-playlist">
       <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
       <Playlist playlistTracks={this.state.playlistTracks} playlistName={this.state.playlistName} 
-      onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+      onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}
+      onSave={this.savePlaylist}/>
     </div>
   </div>
 </div>
---
+++ src/util/Spotify.js
@@ -0,0 +1,65 @@
+import SearchBar from "../Components/SearchBar/SearchBar";
+
+const clientId = '67d62ccd50f9496d9407bc9325af1878';
+const redirectUri = 'http://localhost:3000/';
+let accessToken;
+
+const Spotify = {
+    getAccessToken(){
+        if(accessToken){
+            return accessToken;
+        }
+        //check for Token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }else{
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}
+            &response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    },
+
+    search(keyWord){
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${keyWord}`, {headers:
+            { Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(!jsonResponse){
+                return [];
+            }
+            return jsonResponse.track.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }))
+        })
+    }
+   
+    savePlaylist(playlistName, trackUris){
+        if(!playlistName || !trackUris.length){
+            return;
+        }
+        const accessToken = Spotify.getAccessToken();
+        const headers = {Authorization: `Bearer ${accessToken}`};
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}).then(response => response.json())
+        .then(jsonResponse => {
+            userId = jsonResponse.id;
+        });
+    }
+}
+
+export default Spotify;
